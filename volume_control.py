5f5c30a5ea1425e31e756672942afb479b0044f2
Bakare Tobiloba <bakareoluwatobiloba@gmail.com>
2026-07-12 14:52:53 +0100

    v1.2.1 cleaning up volume control using landmark 0 as a switch for activating the volume control.

--- volume_control.py
+++ volume_control.py
@@ -9,26 +9,30 @@ def main():
     hand_detector = htm.HandDetector()
 
     while True:
+        #setup image capture from webcam
         success, img = cap.read()#reading the image from webcam
         img = hand_detector.findHands(img,bothHands=False)
+
+        #track hand landmarks and get their coordinates
         hand0_landmark_coordinates = hand_detector.findPosition(img, handNo=0, draw=False)
         #hand1_landmark_coordinates = hand_detector.findPosition(img, handNo=1, draw=False)
+        
         img = hand_detector.displayFPS(img)
 
-        #print("Hand 0 Landmark Coordinates:", hand0_landmark_coordinates[4] if hand0_landmark_coordinates else "No hand detected")
-        #print("Hand 1 Landmark Coordinates:", hand1_landmark_coordinates[4] if hand1_landmark_coordinates else "No hand detected")
-
+        
         if hand0_landmark_coordinates:
-            #coordinates of landmarks 4 and 8
-            #print(f"landmark 4 coordinates: {hand0_landmark_coordinates[4] if hand0_landmark_coordinates else 'No hand detected'}")
-            #print(f"landmark 8 coordinates: {hand0_landmark_coordinates[8] if hand0_landmark_coordinates else 'No hand detected'}")
-
-            cv2.line(img, hand0_landmark_coordinates[4], hand0_landmark_coordinates[8], (255, 0, 255), 3)
-            #compute distance between landmarks 4 and 8 if both are detected
-            if (hand0_landmark_coordinates[4][1] < hand0_landmark_coordinates[12][1]) and (hand0_landmark_coordinates[12][1] < hand0_landmark_coordinates[8][1]):
+            #use the base of the palm (landmark 0) as switch for activating the volume control. 
+            # If the palm is above a certain threshold, the volume control will be deactivated.     
+            #print(f"Landmark 0 coordinates: {hand0_landmark_coordinates[0]}")
+            palm_base_y = hand0_landmark_coordinates[0][1]
+            palm_base_threshold = 280  # Adjust this threshold based on your needs
+            if palm_base_y > palm_base_threshold:
                 pass
             else:
+                #compute distance between landmarks 4 and 8 if both are detected
                 if 4 in hand0_landmark_coordinates and 8 in hand0_landmark_coordinates:  
+                    cv2.line(img, hand0_landmark_coordinates[4], hand0_landmark_coordinates[8], (255, 0, 255), 3)
+
                     x4,y4 = hand0_landmark_coordinates[4]
                     x8,y8 = hand0_landmark_coordinates[8]
                     distance = ((x8 - x4) ** 2 + (y8 - y4) ** 2) ** 0.5
@@ -38,16 +42,13 @@ def main():
                     #PYCAW: Adjust volume based on distance between landmarks 4 and 8
                     device = AudioUtilities.GetSpeakers()
                     volume = device.EndpointVolume
-                    #print(f"Audio output: {device.FriendlyName}")
-                    #print(f"- Muted: {bool(volume.GetMute())}")
-                    #print(f"- Volume level: {volume.GetMasterVolumeLevel()} dB")
-                    #print(f"- Volume range: {volume.GetVolumeRange()[0]} dB - {volume.GetVolumeRange()[1]} dB")
                     vol = volume.GetMasterVolumeLevel()
                     vol_step = 1
                     if distance > distance_threshold:
                         vol +=vol_step
                     elif vol < distance_threshold:
                         vol -=vol_step
+
                     clamped_vol = max(volume.GetVolumeRange()[0], min(vol, volume.GetVolumeRange()[1]))
                     volume.SetMasterVolumeLevel(clamped_vol, None)
 

7c67ae57710dc1f4e0cc4a79f706ba04fab01d32
Bakare Tobiloba <bakareoluwatobiloba@gmail.com>
2026-07-15 19:30:18 +0100

    v1.3.1 connected to comp mouse to do: implement kalman filter for better  tracking add calibration per session

--- finger_mouse.py
+++ finger_mouse.py
@@ -6,11 +6,17 @@ screen_width, screen_height = pag.size()  # Get the screen size
 #print(f"Screen size: {screen_width}x{screen_height}")
 left_click_test = [100,100,100]
 right_click_test = [100,100,100]
+mouse_smoothingx = [0,0,0]
+mouse_smoothingy = [0,0,0]
 
+scaling_factor = 8
 rectangle_top_left = (50, 200)
-rectangle_height = 160
-rectangle_width = 300
+rectangle_height = int(screen_height/scaling_factor)
+rectangle_width = int(screen_width/scaling_factor)
+
 rectangle_bottom_right = (rectangle_top_left[0] + rectangle_width, rectangle_top_left[1] + rectangle_height)
+
+
 
 def main():
     #setup webcam capture and hand detector
@@ -28,6 +34,7 @@ def main():
 
         if hand0_landmark_coordinates:
 
+            #finger tracking
             x8, y8 = hand0_landmark_coordinates[8]
             x12,y12 = hand0_landmark_coordinates[12]
             x16,y16 = hand0_landmark_coordinates[16]
@@ -40,11 +47,26 @@ def main():
             
             right_click_test.append(distance2)
             right_click_test.pop(0)
+
+            #smoothing by averaging past 3 frames to reduce jitter
+            mouse_smoothingx.append(x8)
+            mouse_smoothingx.pop(0)
+            mouse_smoothedx = sum(mouse_smoothingx)/len(mouse_smoothingx)
+
+            mouse_smoothingy.append(y8)
+            mouse_smoothingy.pop(0)
+            mouse_smoothedy = sum(mouse_smoothingy)/len(mouse_smoothingy)
+
             
             
             #check if index fingertip is within the rectangle area 
             if rectangle_top_left[0] <= x8 <= rectangle_bottom_right[0] and rectangle_top_left[1] <= y8 <= rectangle_bottom_right[1]:
                 cv2.circle(img, (x8, y8), 10, (0, 255, 0), cv2.FILLED)  # Draw a green circle at landmark 8
+
+                
+                x8_mapped = int(screen_width) - ((mouse_smoothedx - rectangle_top_left[0]) * scaling_factor) #flipped x axis
+                y8_mapped = ((mouse_smoothedy - rectangle_top_left[1]) * scaling_factor)
+                pag.moveTo(x8_mapped,y8_mapped)
             
                 #check for right click
                 if (16 in hand0_landmark_coordinates) and (12 in hand0_landmark_coordinates) and (8 in hand0_landmark_coordinates): #16 is tip of middle finger               
@@ -52,11 +74,13 @@ def main():
                     #if the distances between the landmarks are below a certain threshold for 3 consecutive frames, we can consider it a right click gesture
                     if all(right_click_test[i] < 20 for i in range(len(right_click_test))) and all(left_click_test[i] < 20 for i in range(len(left_click_test))):
                         cv2.putText(img, "Right click Detected", (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                        pag.click(button='right')
                 
                     #check for left click
                     #if the distances between the landmarks are below a certain threshold for 3 consecutive frames, we can consider it a left click gesture                    
                     elif all(left_click_test[i] < 20 for i in range(len(left_click_test))):
                         cv2.putText(img, "Left click Detected", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                        pag.click(button='left')
                 
 
             
